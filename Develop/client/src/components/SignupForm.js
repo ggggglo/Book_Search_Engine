67c13717c6df28ab46458a1cda41721198f092a3
Gloriana Gonzalez <glogonzalez@Glorianas-MacBook-Pro.local>
2023-01-10 11:23:53 -0300

    useEffect for error alerts

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -14,6 +14,14 @@ const SignupForm = () => {
 
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
